7113afb9637694b089703667e2de2fbde98e4e61
ANPHYVARGHESE <157595811+ANPHYVARGHESE@users.noreply.github.com>
2024-03-30 11:07:17 +0400

    Add files via upload

--- courseramod4/controllers/home.controller.js
+++ courseramod4/controllers/home.controller.js
@@ -1,1 +1,13 @@
+(
+    function () {
+        var app = angular.module('menuApp')
+       .controller('HomeController', HomeController);
+        function HomeController() {
+            var ctrl = this;
+            ctrl.title = 'Welcome to Restaurant Application';
+        }
 
+
+
+    }
+)();
